c787655c2896486484fe3805579bc7225d117af1
sharmashekhar0 <beingperfect76@gmail.com>
2023-11-07 20:45:50 +0530

    add footer file

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+	return (
+		<div className="h-4 bg-blue-900 flex items-center justify-center text-1xl p-4 text-white font-bold">
+			<p>
+				© Copyright 2023 - Developed by Shekhar Sharma. All right
+				reserved.
+			</p>
+		</div>
+	);
+}
+
+export default Footer;
