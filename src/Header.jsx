4d0c687ecc881952d849f14fb9c61b3f80f61d0c
sharmashekhar0 <beingperfect76@gmail.com>
2023-11-07 20:45:02 +0530

    add header file

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+	return (
+		<div className="h-8 bg-blue-900 flex items-center justify-center text-3xl p-8 text-white font-extrabold">
+			<h1>75 Days Hard Challenge</h1>
+		</div>
+	);
+}
+
+export default Header;
